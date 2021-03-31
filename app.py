6fa1cfbc6d8e1dc746ddfb46deee7f1301cf36d5
James Richmond <30303752+JimmyBoomBots3000@users.noreply.github.com>
2021-03-31 00:07:59 -0400

    Improve layout, fix map

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 import dash_table
 import pandas as pd
@@ -19,91 +20,122 @@ username = 'testuser'
 password = 'testpassword'
 shelter = AnimalShelter(username, password)
 
-# class read method must support return of cursor object and accept projection json input
+# class read method supports return of cursor object and accepts projection json input
 df = pd.DataFrame.from_records(shelter.read({}))
 
 #########################
 # Dashboard Layout / View
 #########################
-# app = Dash app
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
+app.title = 'Grazioso Salvare'
 
 app.layout = html.Div([
-    # Top section of interface
-    html.Div(id='hidden-div', style={'display': 'none'}),
-    html.Center([
-        html.Table(
-            html.Tr([
-                html.Td(
-                    html.A(
-                        html.Img(src="https://i.ibb.co/CPsh27v/Grazioso-Salvare-Logo.png",
-                                 style={'height': '25%', 'width': '25%'}
-                                 ),
-                        href="https://www.snhu.edu/"
-                    )
-                ),
-                html.Td(html.B('Developed by James Richmond'))]
-            )
-        )]
-    ),
-    html.Hr(),
-    html.Div(
-        # Row containing radio buttons
-        className="row",
-        style={'display': 'flex'},
-        children=[
-            dcc.RadioItems(
-                id='rescue_types',
-                options=[
-                    {'label': 'Water Rescue', 'value': 'water'},
-                    {'label': 'Mountain/ Wilderness Rescue', 'value': 'mountain'},
-                    {'label': 'Disaster/ Individual Tracking', 'value': 'tracking'},
-                    {'label': 'Reset', 'value': 'reset'}
-                ],
-                value='reset',
-                labelStyle={'display': 'inline-block'}
-            )]
-    ),
-    html.Hr(),
-    # Data Table
-    dash_table.DataTable(
-        id='datatable-id',
-        columns=[
-            {"name": i, "id": i, "deletable": False, "selectable": True} for i in df.columns
-        ],
-        data=df.to_dict('records'),
-        editable=False,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable=False,
-        row_selectable="single",
-        row_deletable=False,
-        page_action="native",
-        selected_rows=[],
-        selected_columns=[],
-        page_current=0,
-        page_size=10
-    ),
-    html.Br(),
-    html.Hr(),
-    html.Div(
-        html.Tr([
-            # Pie chart
-            html.Td(
-                dcc.Graph(
-                    id='pie'
-                )
+
+    dbc.Row([
+        dbc.Col(
+            html.A(html.Img(src="https://i.ibb.co/n1X1XgS/img.png", style={'height': '150px'}),
+                   href="https://www.snhu.edu/"
+                   ),
+            width=2,
+            align='center'
+        ),
+        dbc.Col([
+            dbc.Row([
+                dbc.Col(html.H3("Animal Finder Dashboard"),
+                        width={'size': True},
+                        align='center'
+
+                        ),
+                dbc.Col(
+                    html.A(html.Img(src="https://www.python.org/static/community_logos/python-powered-w-70x28.png"),
+                           href="https://www.python.org/about/"
+                           ),
+                    width=1)
+            ]
+
             ),
-            # Map
-            html.Td(
-                id='map-id',
-                className='col s12 m6'
+            dbc.Row([
+                dbc.Col([
+                    html.Hr(),
+                    # Row containing radio buttons
+                    html.Div(
+                        className="row",
+                        style={'display': 'flex'},
+                        children=[
+                            dbc.RadioItems(
+                                id='rescue_types',
+                                options=[
+                                    {'label': 'Water Rescue', 'value': 'water'},
+                                    {'label': 'Mountain/ Wilderness Rescue', 'value': 'mountain'},
+                                    {'label': 'Disaster/ Individual Tracking', 'value': 'tracking'},
+                                    {'label': 'Reset', 'value': 'reset'}
+                                ],
+                                value='reset',
+                            )
+                        ]
+                    ),
+                    html.Hr()
+                ],
+                    width=True),
+            ],
+                justify='left'
+
             )
         ])
-    )
-])
+    ]),
+
+    dbc.Row([
+        dbc.Col(
+            # Data Table
+            dash_table.DataTable(
+                id='datatable-id',
+                columns=[
+                    {"name": i, "id": i, "deletable": False, "selectable": True} for i in df.columns
+                ],
+                data=df.to_dict('records'),
+                editable=False,
+                filter_action="native",
+                sort_action="native",
+                sort_mode="multi",
+                column_selectable=False,
+                row_selectable="single",
+                row_deletable=False,
+                page_action="native",
+                selected_rows=[],
+                selected_columns=[],
+                page_current=0,
+                page_size=10,
+                style_cell={
+                    'overflow': 'hidden',
+                    'textOverflow': 'ellipsis',
+                    'maxWidth': 0
+                }
+            ),
+            width=12
+        )
+    ]),
+    dbc.Row([
+        dbc.Col(
+            # Pie Chart
+            dcc.Graph(id='pie'),
+            width=6
+        ),
+        dbc.Col(
+            # Map
+            dl.Map([dl.TileLayer(), dl.LayerGroup(id="layer")],
+                   id="map-id",
+                   style={'width': '100%', 'height': '50vh', 'margin': "auto", "display": "block"},
+                   center=[30.31880634, -97.72403767],
+                   zoom=5
+                   ),
+            width=6
+        ),
+    ]),
+],
+    style={'width': '98%'}
+
+)
 
 
 #############################################
@@ -112,46 +144,45 @@ app.layout = html.Div([
 # Callback for map
 # Changes map location focus and tooltips per selected row of table
 @app.callback(
-    Output('map-id', "children"),
-    [Input('datatable-id', "derived_viewport_data"),
-     Input('datatable-id', "selected_rows")])
+    [
+        Output('layer', "children"),
+        Output('map-id', 'center'),
+        Output('map-id', 'zoom')
+    ],
+    [
+        Input('datatable-id', "derived_viewport_data"),
+        Input('datatable-id', "selected_rows")
+    ]
+)
 # Calback inputs are passed as arguments
 def update_map(viewData, row):
     dff = pd.DataFrame.from_dict(viewData)
     lat = float(dff.iloc[row, 14])
     long = float(dff.iloc[row, 15])
-    return [
-        dl.Map(
-            style={
-                'width': '750px',
-                'height': '500px'
-            },
-            # Sets the center of the map
-            center=[
+
+    children = [
+        # Marker with tool tip and popup
+        dl.TileLayer(id="base-layer-id"),
+        dl.Marker(
+            position=[
                 lat,
                 long
             ],
-            zoom=10,
             children=[
-                dl.TileLayer(id="base-layer-id"),
-                # Marker with tool tip and popup
-                dl.Marker(
-                    position=[
-                        lat,
-                        long
-                    ],
-                    children=[
-                        dl.Tooltip(
-                            dff.iloc[row, 4]),  # animal type
-                        dl.Popup([
-                            html.H2(dff.iloc[row, 10]),  # animal name
-                            html.H3(dff.iloc[row, 5])  # animal breed
-                        ])
-                    ]
-                )
+                dl.Tooltip(
+                    dff.iloc[row, 4]),  # animal type
+                dl.Popup([
+                    html.H3(dff.iloc[row, 10]),  # animal name
+                    html.H4(dff.iloc[row, 5])  # animal breed
+                ])
             ]
         )
     ]
+
+    # Center map on selection
+    center = [lat, long]
+
+    return children, center, 10
 
 
 # Callback for radio buttons
@@ -236,7 +267,6 @@ def generate_chart(data):
     fig = px.pie(
         dff,
         names='breed',
-        title='Animals by Breed (Selection)'
     )
 
     return fig
